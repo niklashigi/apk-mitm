127b89eb96ad67f97c372230cbdf97ad68931b80
Niklas Higi <niklas@shroudedcode.com>
2019-10-12 12:26:47 +0200

    Remove "I: " prefix from Apktool logs

--- src/tools/uber-apk-signer.ts
+++ src/tools/uber-apk-signer.ts
@@ -1,13 +1,14 @@
 import * as path from 'path'
 
 import executeJar from '../utils/execute-jar'
+import observeProcess from '../utils/observe-process'
 
 const jar = path.join(__dirname, '../../jar/uber-apk-signer.jar')
 
 const uberApkSigner = {
-  sign(inputPath: string) {
-    return executeJar(jar, ['--apks', inputPath])
-  },
+  sign: (inputPath: string) => observeProcess(
+    executeJar(jar, ['--apks', inputPath]),
+  ),
   version: 'v1.1.0',
 }
 
