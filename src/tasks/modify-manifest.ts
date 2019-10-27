a4bf34549e3a08f859dba2f73b8c9f3e01eb2875
Niklas Higi <niklas@shroudedcode.com>
2019-10-27 16:05:39 +0100

    Use XML compact mode for modifying manifest

--- src/tasks/modify-manifest.ts
+++ src/tasks/modify-manifest.ts
@@ -2,22 +2,24 @@ import * as fs from '../utils/fs'
 import xml from 'xml-js'
 
 export default async function modifyManifest(path: string) {
-  const manifest = xml.xml2js(await fs.readFile(path, 'utf-8'))
+  const fileXml = xml.xml2js(
+    await fs.readFile(path, 'utf-8'),
+    { compact: true, alwaysArray: true },
+  )
+  const manifest = fileXml['manifest'][0]
+  const application = manifest['application'][0]
 
-  const application = manifest.elements[0].elements
-    .filter((element: any) => element.name === 'application')[0]
-
-  application.attributes['android:debuggable'] = 'true'
+  application._attributes['android:debuggable'] = 'true'
 
   let nscName = 'network_security_config'
-  const nscReference: string = application.attributes['android:networkSecurityConfig']
+  const nscReference: string = application._attributes['android:networkSecurityConfig']
   if (nscReference && nscReference.startsWith('@xml/')) {
     nscName = nscReference.slice(5)
   } else {
-    application.attributes['android:networkSecurityConfig'] = '@xml/network_security_config'
+    application._attributes['android:networkSecurityConfig'] = `@xml/${nscName}`
   }
 
-  await fs.writeFile(path, xml.js2xml(manifest, { spaces: 4 }))
+  await fs.writeFile(path, xml.js2xml(fileXml, { compact: true, spaces: 4 }))
 
   return { nscName }
 }
