6f0c2e14848299678ad32e2f8255a462c8f8b81a
Jérémie Zarca <jeremie.zarca@gmail.com>
2019-10-30 12:57:41 +0100

    requested changes

--- src/cli.ts
+++ src/cli.ts
@@ -1,32 +1,45 @@
-import chalk from 'chalk'
-import parseArgs from 'yargs-parser'
+import chalk from "chalk";
+import parseArgs from "yargs-parser";
+import path from "path";
 
-import prepareApk from '.'
+import { prepareApk, prepareAppBundle } from ".";
 
 async function main() {
   const args = parseArgs(process.argv.slice(2), {
-    string: ['apktool'],
-  })
+    string: ["apktool"]
+  });
 
   if (args.help) {
-    showHelp()
-    process.exit()
+    showHelp();
+    process.exit();
   }
 
-  const [apkPath] = args._
+  const [apkPath] = args._;
   if (!apkPath) {
-    showHelp()
-    process.exit(1)
+    showHelp();
+    process.exit(1);
   }
-
-  prepareApk(apkPath, { apktoolPath: args.apktool })
+  const fileExtension: string = path.extname(apkPath);
+  switch(fileExtension) {
+    case 'apk':
+      prepareApk(apkPath, { apktoolPath: args.apktool });
+      break;
+    case 'xapk':
+      prepareAppBundle(apkPath, { apktoolPath: args.apktool });
+      break;
+    case 'apks':
+      prepareAppBundle(apkPath, { apktoolPath: args.apktool });
+      break;
+    default:
+      break;
+  }
 }
 
 function showHelp() {
   console.log(chalk`
   $ {bold apk-mitm} <path-to-apk>
       {dim {bold --apktool} Path to custom Apktool.jar {gray.italic (optional)}}
-    `)
+    `);
 }
 
-main()
+main();
--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,19 @@ type Options = {
   apktoolPath?: string;
 };
 
-export default async function prepareApk(apkPath: string, options: Options) {
+const TMP_DIR = tempy.directory();
+
+const DECODE_DIR = path.join(TMP_DIR, "decode");
+const UNSIGNED_APK_PATH = path.join(TMP_DIR, "unsigned.apk");
+
+export async function prepareApk(apkPath: string, options: Options) {
   const apktool = new Apktool(options.apktoolPath);
+  const finishedApkName = `${path.basename(apkPath, ".apk")}-patched.apk`;
+  const finishedApkPath = path.join(path.dirname(apkPath), finishedApkName);
+  let fallBackToAapt = false;
+  let nscName: string;
+
+  apkPath = path.resolve(process.cwd(), apkPath);
 
   console.log(chalk`
   {dim ╭} {blue {bold apk-mitm} v${version}}
@@ -32,46 +43,32 @@ export default async function prepareApk(apkPath: string, options: Options) {
   ╰ {bold uber-apk-signer} ${uberApkSigner.version}}
   `);
 
-  apkPath = path.resolve(process.cwd(), apkPath);
-
-  const finishedApkName = `${path.basename(apkPath, ".apk")}-patched.apk`;
-  const finishedApkPath = path.join(path.dirname(apkPath), finishedApkName);
-
-  const tmpDir = tempy.directory();
-  console.log(chalk.dim(`  Using temporary directory:\n  ${tmpDir}\n`));
-
-  const decodeDir = path.join(tmpDir, "decode");
-  const unsignedApkPath = path.join(tmpDir, "unsigned.apk");
-
-  let fallBackToAapt = false;
-  let usesAppBundle = false;
-  let nscName: string;
+  console.log(chalk.dim(`  Using temporary directory:\n  ${TMP_DIR}\n`));
 
   await new Listr([
     {
       title: "Decoding APK file",
-      task: () => apktool.decode(apkPath, decodeDir)
+      task: () => apktool.decode(apkPath, DECODE_DIR)
     },
     {
       title: "Modifying app manifest",
       task: async () => {
         const result = await modifyManifest(
-          path.join(decodeDir, "AndroidManifest.xml")
+          path.join(DECODE_DIR, "AndroidManifest.xml")
         );
         nscName = result.nscName;
-        usesAppBundle = result.usesAppBundle;
       }
     },
     {
       title: "Modifying network security config",
       task: () =>
         modifyNetworkSecurityConfig(
-          path.join(decodeDir, `res/xml/${nscName}.xml`)
+          path.join(DECODE_DIR, `res/xml/${nscName}.xml`)
         )
     },
     {
       title: "Disabling certificate pinning",
-      task: (_, task) => disableCertificatePinning(decodeDir, task)
+      task: (_, task) => disableCertificatePinning(DECODE_DIR, task)
     },
     {
       title: "Encoding patched APK file",
@@ -81,7 +78,7 @@ export default async function prepareApk(apkPath: string, options: Options) {
             title: "Encoding using AAPT2",
             task: (_, task) =>
               new Observable(subscriber => {
-                apktool.encode(decodeDir, unsignedApkPath, true).subscribe(
+                apktool.encode(DECODE_DIR, UNSIGNED_APK_PATH, true).subscribe(
                   line => subscriber.next(line),
                   () => {
                     subscriber.complete();
@@ -95,7 +92,7 @@ export default async function prepareApk(apkPath: string, options: Options) {
           {
             title: chalk`Encoding using AAPT {dim [fallback]}`,
             skip: () => !fallBackToAapt,
-            task: () => apktool.encode(decodeDir, unsignedApkPath, false)
+            task: () => apktool.encode(DECODE_DIR, UNSIGNED_APK_PATH, false)
           }
         ])
     },
@@ -105,11 +102,11 @@ export default async function prepareApk(apkPath: string, options: Options) {
         new Observable(subscriber => {
           (async () => {
             await uberApkSigner
-              .sign(unsignedApkPath)
+              .sign(UNSIGNED_APK_PATH)
               .forEach(line => subscriber.next(line));
 
             await fs.copyFile(
-              path.join(tmpDir, "unsigned-aligned-debugSigned.apk"),
+              path.join(TMP_DIR, "unsigned-aligned-debugSigned.apk"),
               finishedApkPath
             );
 
@@ -128,107 +125,127 @@ export default async function prepareApk(apkPath: string, options: Options) {
       process.exit(1);
     });
 
-  if (usesAppBundle) {
-    console.log(
-      chalk`{yellow
+  console.log(chalk`
+  {green.inverse  Done! } Patched APK: {bold ./${finishedApkName}}
+  `);
+}
+
+export async function prepareAppBundle(apkPath: string, options: Options) {
+  const apktool = new Apktool(options.apktoolPath);
+  apkPath = path.resolve(process.cwd(), apkPath);
+
+  console.log(
+    chalk`{yellow
   {inverse.bold  WARNING }
 
   This app seems to be using {bold Android App Bundle} which is dependent on Google Play.
-  This feature is very {bold experimental} as a result {bold it might not work}`
-    );
+  This feature is very {bold experimental} as a result {bold it might not work} 
+  }`
+  );
 
-    console.log(chalk`
+  console.log(chalk`
     {dim ╭} {blue {bold apk-mitm} v${version}}
     {dim ├ {bold apktool} ${apktool.version}
     ╰ {bold uber-apk-signer} ${uberApkSigner.version}}
     `);
 
-    console.log(chalk.dim(`  Using temporary directory:\n  ${tmpDir}\n`));
+  console.log(chalk.dim(`  Using temporary directory:\n  ${tmpDir}\n`));
 
-    await new Listr(
-      [
-        {
-          title: "Unzipping App Bundle",
-          task: () => compression.unzip(apkPath, decodeDir)
-        },
-        {
-          title: "Generating a new signing key",
-          task: () => keytool.createCertificate()
-        },
-        {
-          title: "Decoding base.apk file",
-          task: (_, task) =>
-            new Observable(subscriber => {
-              const baseApkFile: string = path.join(decodeDir, "base.apk");
-              this.prepareApk(baseApkFile, unsignedApkPath, true).subscribe(
-                line => subscriber.next(line),
-                () => {
-                  subscriber.complete();
-                  throw new Error("Error with decoding base.apk");
-                },
-                () => subscriber.complete()
-              );
-            })
-        },
-        {
-          title: "Replacing patched base.apk",
-          task: async () => {
-            const baseApkFile: string = path.join(decodeDir, "base.apk");
-            const basePatchedApkFile: string = path.join(
-              decodeDir,
-              "base-patched.apk"
-            );
-            await fs.unlink(baseApkFile);
-            await fs.rename(basePatchedApkFile, baseApkFile);
-          }
-        },
-        {
-          title: "Signing APK file",
-          task: () =>
-            new Observable(subscriber => {
-              async () => {
-                const apkFiles = await globby(path.join(decodeDir, "**/*.apk"));
-
-                for (const filePath of apkFiles) {
-                  await executeBin('apksigner', [
-                    "sign",
-                    "--ks debug.keystore", 
-                    "--ks-pass pass:android",
-                    filePath
-                  ])
-                }
-
-                subscriber.complete();
-              };
-            })
-        },
-        {
-          title: "Zipping all the apk files",
-          task: async () => {
-            const bundleExtension: string = path.extname(apkPath);
-            const finishedBundleName: string = `${path.basename(apkPath, bundleExtension)}-patched.${bundleExtension}`;
-            const finishedBundlePath: string = path.join(path.dirname(apkPath), finishedBundleName);
-            const apkFiles = await globby(path.join(decodeDir, "**/*.apk"));
-            await compression.zip(finishedBundlePath, apkFiles);
-          }
-        },
-      ],
+  await new Listr(
+    [
       {
-        exitOnError: true
+        title: "Unzipping App Bundle",
+        task: () => compression.unzip(apkPath, DECODE_DIR)
+      },
+      {
+        title: "Generating a new signing key",
+        task: () => keytool.createCertificate()
+      },
+      {
+        title: "Doing some magic over base.apk",
+        task: () =>
+          new Listr([
+            {
+              title: "Preparing APK",
+              task: (_, task) =>
+                new Observable(subscriber => {
+                  const baseApkFile: string = path.join(DECODE_DIR, "base.apk");
+                  this.prepareApk(
+                    baseApkFile,
+                    UNSIGNED_APK_PATH,
+                    true
+                  ).subscribe(
+                    line => subscriber.next(line),
+                    () => {
+                      subscriber.complete();
+                      throw new Error("Error with preparing base.apk");
+                    },
+                    () => subscriber.complete()
+                  );
+                })
+            }
+          ])
+      },
+      {
+        title: "Replacing patched base.apk",
+        task: async () => {
+          const baseApkFile: string = path.join(DECODE_DIR, "base.apk");
+          const basePatchedApkFile: string = path.join(
+            DECODE_DIR,
+            "base-patched.apk"
+          );
+          await fs.unlink(baseApkFile);
+          await fs.rename(basePatchedApkFile, baseApkFile);
+        }
+      },
+      {
+        title: "Signing APK file",
+        task: () =>
+          new Observable(subscriber => {
+            async () => {
+              const apkFiles = await globby(path.join(DECODE_DIR, "**/*.apk"));
+
+              for (const filePath of apkFiles) {
+                await executeBin("apksigner", [
+                  "sign",
+                  "--ks debug.keystore",
+                  "--ks-pass pass:android",
+                  filePath
+                ]);
+              }
+
+              subscriber.complete();
+            };
+          })
+      },
+      {
+        title: "Zipping all the apk files",
+        task: async () => {
+          const bundleExtension: string = path.extname(apkPath);
+          const finishedBundleName: string = `${path.basename(
+            apkPath,
+            bundleExtension
+          )}-patched.${bundleExtension}`;
+          const finishedBundlePath: string = path.join(
+            path.dirname(apkPath),
+            finishedBundleName
+          );
+          const apkFiles = await globby(path.join(DECODE_DIR, "**/*.apk"));
+          await compression.zip(finishedBundlePath, apkFiles);
+        }
       }
-    )
-      .run()
-      .catch(error => {
-        console.error(
-          chalk`\n  {red.inverse.bold  Failed! } An error occurred:\n\n`,
-          error.toString()
-        );
+    ],
+    {
+      exitOnError: true
+    }
+  )
+    .run()
+    .catch(error => {
+      console.error(
+        chalk`\n  {red.inverse.bold  Failed! } An error occurred:\n\n`,
+        error.toString()
+      );
 
-        process.exit(1);
-      });
-
-    console.log(chalk`
-  {green.inverse  Done! } Patched APK: {bold ./${finishedApkName}}
-  `);
-  }
+      process.exit(1);
+    });
 }
