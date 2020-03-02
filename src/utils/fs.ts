cf59952753716f29b06177c16b3ef8b261e25d5b
Niklas Higi <niklas@shroudedcode.com>
2020-03-02 20:00:36 +0100

    Use native Promise versions of `fs` functions where possible

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,10 +1,11 @@
 import * as fs from 'fs'
+import { promises as fsp } from 'fs'
 import { promisify } from 'util'
 
 export const readFile = promisify(fs.readFile)
-export const writeFile = promisify(fs.writeFile)
-export const copyFile = promisify(fs.copyFile)
+export const writeFile = fsp.writeFile
+export const copyFile = fsp.copyFile
 export const exists = promisify(fs.exists)
-export const unlink = promisify(fs.unlink)
-export const rename = promisify(fs.rename)
-export const mkdir = promisify(fs.mkdir)
+export const unlink = fsp.unlink
+export const rename = fsp.rename
+export const mkdir = fsp.mkdir
