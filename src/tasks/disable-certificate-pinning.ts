0856cdac2ec7f41f475c59f934332293044753a8
Niklas Higi <niklas@shroudedcode.com>
2020-08-08 16:44:27 +0200

    Disable certificate pinning more reliably
    FIxes #22

--- src/tasks/disable-certificate-pinning.ts
+++ src/tasks/disable-certificate-pinning.ts
@@ -2,17 +2,38 @@ import * as path from 'path'
 import * as fs from '../utils/fs'
 
 import globby from 'globby'
-import replaceAll from 'replace-string'
+import escapeStringRegexp from 'escape-string-regexp'
 import { Observable } from 'rxjs'
 import { ListrTaskWrapper } from 'listr'
 
-const methodSignatures = [
-  '.method public checkClientTrusted([Ljava/security/cert/X509Certificate;Ljava/lang/String;)V',
-  '.method public final checkClientTrusted([Ljava/security/cert/X509Certificate;Ljava/lang/String;)V',
-  '.method public checkServerTrusted([Ljava/security/cert/X509Certificate;Ljava/lang/String;)V',
-  '.method public final checkServerTrusted([Ljava/security/cert/X509Certificate;Ljava/lang/String;)V',
-  '.method public getAcceptedIssuers()[Ljava/security/cert/X509Certificate;',
-  '.method public final getAcceptedIssuers()[Ljava/security/cert/X509Certificate;',
+/** The methods that need to be patched to disable certificate pinning. */
+const METHOD_SIGNATURES = [
+  'checkClientTrusted([Ljava/security/cert/X509Certificate;Ljava/lang/String;)V',
+  'checkServerTrusted([Ljava/security/cert/X509Certificate;Ljava/lang/String;)V',
+  'getAcceptedIssuers()[Ljava/security/cert/X509Certificate;',
+]
+
+/** Patterns used to find the methods defined in `METHOD_SIGNATURES`. */
+const METHOD_PATTERNS = METHOD_SIGNATURES.map(signature => {
+  const escapedSignature = escapeStringRegexp(signature)
+  return new RegExp(
+    `(\\.method public (?:final )?${escapedSignature})\\n([^]+?)\\n(\\.end method)`,
+    'g',
+  )
+})
+
+/** Code inserted into `checkClientTrusted` and `checkServerTrusted`. */
+const RETURN_VOID_FIX = [
+  '.locals 0',
+  'return-void',
+]
+
+/** Code inserted into `getAcceptedIssuers`. */
+const RETURN_EMPTY_ARRAY_FIX = [
+  '.locals 1',
+  'const/4 v0, 0x0',
+  'new-array v0, v0, [Ljava/security/cert/X509Certificate;',
+  'return-object v0',
 ]
 
 export default async function disableCertificatePinning(directoryPath: string, task: ListrTaskWrapper) {
@@ -29,11 +50,37 @@ export default async function disableCertificatePinning(directoryPath: string, t
         const originalContent = await fs.readFile(filePath, 'utf-8')
         let patchedContent = originalContent
 
-        for (const signature of methodSignatures) {
-          patchedContent = replaceAll(
-            patchedContent,
-            signature,
-            `${signature}\n    return-void`,
+        for (const pattern of METHOD_PATTERNS) {
+          patchedContent = patchedContent.replace(
+            pattern, (
+              _,
+              openingLine: string,
+              body: string,
+              closingLine: string,
+            ) => {
+              const bodyLines = body
+                .split('\n')
+                .map(line => line.replace(/^    /, ''))
+
+              const fixLines = openingLine.includes('getAcceptedIssuers')
+                ? RETURN_EMPTY_ARRAY_FIX
+                : RETURN_VOID_FIX
+
+              const patchedBodyLines = [
+                '# inserted by apk-mitm to disable certificate pinning',
+                ...fixLines,
+                '',
+                '# commented out by apk-mitm to disable old method body',
+                '# ',
+                ...bodyLines.map(line => `# ${line}`)
+              ]
+
+              return [
+                openingLine,
+                ...patchedBodyLines.map(line => `    ${line}`),
+                closingLine,
+              ].map(line => line.trimEnd()).join('\n')
+            },
           )
         }
 
