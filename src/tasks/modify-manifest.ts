e6f291c757c64a1702d6cce2c92b2e2943bf0849
Niklas Higi <niklas@shroudedcode.com>
2021-01-23 17:11:06 +0100

    Stop using `xml-js` compact mode for modifying manifest
    Compact mode doesn't preserve the order of elements in the file and
    produces unnecessarily large diffs as a result.

--- src/tasks/modify-manifest.ts
+++ src/tasks/modify-manifest.ts
@@ -2,23 +2,24 @@ import * as fs from '../utils/fs'
 import xml = require('xml-js')
 
 export default async function modifyManifest(path: string) {
-  const fileXml: xml.ElementCompact = xml.xml2js(
-    await fs.readFile(path, 'utf-8'),
-    { compact: true, alwaysArray: true },
-  )
-  const manifest = fileXml['manifest'][0]
-  const application = manifest['application'][0]
+  const document = xml.xml2js(await fs.readFile(path, 'utf-8')) as xml.Element
 
-  application._attributes['android:networkSecurityConfig'] = '@xml/nsc_mitm'
+  const manifest = document.elements?.find(el => el.name === 'manifest')!
+  const application = manifest.elements?.find(el => el.name === 'application')!
+
+  application.attributes = {
+    ...application.attributes,
+    'android:networkSecurityConfig': '@xml/nsc_mitm',
+  }
 
   const usesAppBundle =
-    application['meta-data'] &&
-    application['meta-data'].some(
-      (meta: any) =>
-        meta._attributes['android:name'] === 'com.android.vending.splits',
-    )
+    application.elements?.some(
+      el =>
+        el.name === 'meta-data' &&
+        el.attributes?.['android:name'] === 'com.android.vending.splits',
+    ) ?? false
 
-  await fs.writeFile(path, xml.js2xml(fileXml, { compact: true, spaces: 4 }))
+  await fs.writeFile(path, xml.js2xml(document, { spaces: 4 }))
 
   return { usesAppBundle }
 }
